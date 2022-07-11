369151d4383edf3e38233424437e9b1cfa6ac96a
warspitenavy <dev@warspite.navy>
2022-07-11 22:56:20 +0900

    metaタグ追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,23 @@ useHead({
   bodyAttrs: {
     class: 'biz-udpgothic'
   },
+  meta: [
+    {
+      charset: 'utf-8'
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    },
+    {
+      name: 'description',
+      content: 'こんにちは、漆原です。'
+    },
+    {
+      name: 'keywords',
+      content: '漆原, urshbara'
+    }
+  ],
   title: '漆原のホームページ'
 })
 </script>
